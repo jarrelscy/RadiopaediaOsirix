68e4235b1b04ed6f0bc17dbd7125090eb777b290
Jarrel Seah <Administrator@jarrels-mac-pro.local>
2016-04-07 22:53:00 +1000

    more pods

--- RadiopaediaFilter.h
+++ RadiopaediaFilter.h
@@ -9,9 +9,11 @@
 #import <OsiriXAPI/PluginFilter.h>
 
 @interface RadiopaediaFilter : PluginFilter {
-
+    NSMutableArray *selectedSeries;
 }
+@property  (nonatomic, strong) NSMutableArray *selectedSeries; // needs to be accessible in the block and also make sure ARC doesn't zombify the thing 
 
+-(void) alert:(NSArray *)info;
 - (long) filterImage:(NSString*) menuName;
-
+- (void) processSeries:(DicomSeries*) series with:(NSString *)caseId;
 @end
--- RadiopaediaFilter.m
+++ RadiopaediaFilter.m
@@ -7,32 +7,136 @@
 
 #import "RadiopaediaFilter.h"
 #import "GTMOAuth2WindowController.h"
+#import <OsiriXAPI/DicomStudy.h>
+#import <OsiriXAPI/DicomSeries.h>
+#import <OsiriXAPI/DicomImage.h>
+#import <OsiriXAPI/browserController.h>
+#import "GTMOAuth2SignIn.h"
 @implementation RadiopaediaFilter
 
 - (void) initPlugin
 {
 }
+
+-(void) alert:(NSArray *)info
+{
+    
+    NSAlert *myAlert = [NSAlert alertWithMessageText:[info objectAtIndex:0]
+                                       defaultButton:[info objectAtIndex:1]
+                                     alternateButton:[info objectAtIndex:2]
+                                         otherButton:[info objectAtIndex:3]
+                           informativeTextWithFormat:[info objectAtIndex:4]];
+    [myAlert runModal];
+}
 - (void)viewController:(GTMOAuth2WindowController *)viewController
       finishedWithAuth:(GTMOAuth2Authentication *)auth
                  error:(NSError *)error {
     if (error != nil) {
-        NSAlert *myAlert = [NSAlert alertWithMessageText:@"Hello World!"
-                                           defaultButton:@"Hello"
-                                         alternateButton:nil
-                                             otherButton:nil
-                               informativeTextWithFormat:@"Failed %ld", (long)[error code]];
         
-        [myAlert runModal];
     } else {
-        NSAlert *myAlert = [NSAlert alertWithMessageText:@"Hello World!"
-                                           defaultButton:@"Hello"
-                                         alternateButton:nil
-                                             otherButton:nil
-                               informativeTextWithFormat:@"%Success"];
         
-        [myAlert runModal];
+        
+        // TODO ASK THE USER WHAT THE TITLE AND SYSTEM ID IS
+        
+        NSMutableDictionary *paramsDict = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                                           @"Jarrel Plugin Test", @"title",
+                                           @"1", @"system_id",
+                                           nil];
+        NSString *paramStr = [GTMOAuth2Authentication encodedQueryParametersForDictionary:paramsDict];
+        NSMutableURLRequest *request  = [GTMOAuth2SignIn mutableURLRequestWithURL:[NSURL URLWithString:@"http://sandbox.radiopaedia.org/api/v1/cases"]
+                                      paramString:paramStr];
+        request.HTTPMethod = @"POST";
+        
+        
+        [auth authorizeRequest:request completionHandler:^(NSError *err)
+         {
+             
+             if (err == nil) {
+                 // the request has been authorized
+                 
+                 NSError *requestError = nil;
+                 NSURLResponse *urlResponse = nil;
+                 NSData *response1 =
+                 [NSURLConnection sendSynchronousRequest:request
+                                       returningResponse:&urlResponse error:&requestError];
+                 NSDictionary *jsonArray = [NSJSONSerialization JSONObjectWithData:response1 options: NSJSONReadingMutableContainers error: &requestError];
+                 
+                 NSString *caseId = [jsonArray objectForKey:@"id"];
+                 NSMutableArray *seriesNames = [NSMutableArray array];
+                 for (DicomSeries *series in self.selectedSeries) {
+                     [self processSeries:series with:caseId using:auth];
+                     [seriesNames addObject:[series name]];
+                 }
+                 
+             }
+             
+             else{
+                 // Failed to authorize for some reason
+                 NSAlert *myAlert = [NSAlert alertWithMessageText:@"Hello World!"
+                                                    defaultButton:@"Hello"
+                                                  alternateButton:nil
+                                                      otherButton:nil
+                                        informativeTextWithFormat:@"Failed %ld", (long)[error code]];
+                 
+                 [myAlert performSelectorOnMainThread:@selector(runModal) withObject:nil waitUntilDone:NO];
+             }
+
+             
+         }];
+        
     }
 }
+
+
+- (void) processSeries:(DicomSeries*) series with:(NSString *)caseId using:(GTMOAuth2Authentication *)auth {
+    NSMutableDictionary *paramsDict = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                                       [series modality], @"modality",
+                                       [series name], @"caption",
+                                       nil];
+    NSString *paramStr = [GTMOAuth2Authentication encodedQueryParametersForDictionary:paramsDict];
+    NSString *urlString = [NSString stringWithFormat:@"http://sandbox.radiopaedia.org/api/v1/cases/%@/studies", caseId];
+    NSMutableURLRequest *request  = [GTMOAuth2SignIn mutableURLRequestWithURL:[NSURL URLWithString:urlString]
+                                                                  paramString:paramStr];
+    request.HTTPMethod = @"POST";
+    [auth authorizeRequest:request completionHandler:^(NSError *err)
+     {
+         
+         if (err == nil) {
+             // the request has been authorized
+             
+             NSError *requestError = nil;
+             NSURLResponse *urlResponse = nil;
+             NSData *response1 =
+             [NSURLConnection sendSynchronousRequest:request
+                                   returningResponse:&urlResponse error:&requestError];
+             NSDictionary *jsonArray = [NSJSONSerialization JSONObjectWithData:response1 options: NSJSONReadingMutableContainers error: &requestError];
+             
+             NSString *studyId = [jsonArray objectForKey:@"id"];
+             
+             
+             // Post images
+             
+             
+             
+         }
+         
+         else{
+             // Failed to authorize for some reason
+             NSAlert *myAlert = [NSAlert alertWithMessageText:@"Error uploading series!"
+                                                defaultButton:@"Ok"
+                                              alternateButton:nil
+                                                  otherButton:nil
+                                    informativeTextWithFormat:@"Failed to upload %@: %@",[series name], [err description]];
+             
+             [myAlert runModal];
+         }
+         
+         
+     }];
+
+    
+}
+
 - (long) filterImage:(NSString*) menuName
 {
    /* NSString* message = [[NSUserDefaults standardUserDefaults] stringForKey:@"HelloWorld_Message"];
@@ -46,6 +150,49 @@
     
     [myAlert runModal];*/
 
+    /*
+     
+     Many thanks to wonderful blog at http://myfirstosirixplugin.blogspot.com.au/   !!!!
+     
+     */
+    
+    BrowserController *currentBrowser = [BrowserController currentBrowser];
+    NSArray *selectedItems = [currentBrowser databaseSelection];
+    
+    if ([selectedItems count] == 0) {
+        NSAlert *alert = [[[NSAlert alloc] init] autorelease];
+        [alert setMessageText:@"No studies/series selected!"];
+        [alert runModal];
+        
+        return 1;
+    }    
+    self.selectedSeries = [NSMutableArray array];
+    for (id item in selectedItems) {
+        if ([item isKindOfClass:[DicomStudy class]]) {
+            DicomStudy *study = (DicomStudy*) item;
+            
+            for (DicomSeries *series in [study imageSeries])
+                [self.selectedSeries addObject:series];
+            
+        } else if ([item isKindOfClass:[DicomSeries class]])
+            [self.selectedSeries addObject:(DicomSeries*) item];
+    }
+    
+    
+    // Process dicom series
+    NSNumber *compressionFactor = [NSNumber numberWithFloat:0.9];
+    NSDictionary *imageProps = [NSDictionary dictionaryWithObject:compressionFactor
+                                                           forKey:NSImageCompressionFactor];
+    for (DicomSeries *series in self.selectedSeries)
+    {
+        for (DicomImage *image in [series sortedImages])
+        {
+            NSImageRep *imageRep = [[[image image] representations] objectAtIndex:0];
+            NSData *imageData = [imageRep representationUsingType:NSJPEGFileType properties:imageProps];
+            [[image instanceNumber] stringValue];
+        }
+        
+    }
     
     
     NSURL *tokenURL = [NSURL URLWithString:@"http://sandbox.radiopaedia.org/oauth/token"];
@@ -56,7 +203,6 @@
     NSString *redirectURI = @"urn:ietf:wg:oauth:2.0:oob";
     
     GTMOAuth2Authentication *auth;
-    @try {
     auth = [GTMOAuth2Authentication authenticationWithServiceProvider:@"Radiopaedia"
                                                              tokenURL:tokenURL
                                                           redirectURI:redirectURI
@@ -76,19 +222,10 @@
         [windowController signInSheetModalForWindow:[NSApp keyWindow]
                                            delegate:self
                                    finishedSelector:@selector(viewController:finishedWithAuth:error:)];
-    }
+       
+    // Here we try to retrieve the cases
     
-    @catch (NSException *exception)
-    {
-        NSAlert *myAlert = [NSAlert alertWithMessageText:@"Hello World!"
-                                           defaultButton:@"Hello"
-                                         alternateButton:nil
-                                             otherButton:nil
-                               informativeTextWithFormat:@"%@", exception];
-        
-        [myAlert runModal];
-    }
-    
+   
     return 0;
 
 }
